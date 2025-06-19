37237fc2701855238c1d9943102ddd903d3870f5
EddyGiusepe <eddychirinos.unac@gmail.com>
2025-06-19 12:31:14 -0300

    Script que é um servidor para CUMPRIMENTAR as pessoas (greeter)

--- MCP_greeter/greeter.py
+++ MCP_greeter/greeter.py
@@ -1,12 +1,20 @@
+#! /usr/bin/env python3
+"""
+Senior Data Scientist.: Dr. Eddy Giusepe Chirinos Isidro
+
+Este é um servidor MCP que retorna uma mensagem de boas-vindas.
+"""
 from mcp.server.fastmcp import FastMCP
 
 # Cria um servidor MCP nomeado "Greeter"
 mcp = FastMCP("Greeter")
 
+
 @mcp.tool()
 def greet() -> str:
     """Retorna esta mensagem de boas-vindas, quando cumprimentado com "Olá", "Oi" ou "Hey"."""
     return "Olá Eddy Giusepe, Bem-vindo ao mundo dos MCPs!"
+
 
 if __name__ == "__main__":
     mcp.run()
